b90527f0cf7b954f15f75bd6477945e162d72aca
ryan255 <ryan255@163.com>
2016-08-01 20:14:34 +0800

    EXAM2

---
+++ EXAM2/src/main/java/com/hand/Client.java
@@ -0,0 +1,103 @@
+package com.hand;
+
+/**
+ * Created by ryan255 on 16/8/1.
+ */
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+
+public class Client {
+    private ClientSocket cs = null;
+
+    private String ip = "127.0.0.1";// 设置成服务器IP
+
+    private int port = 12345;
+
+    private String sendMessage = "hello";
+
+    public Client() {
+        try {
+            if (createConnection()) {
+                sendMessage();
+                getMessage();
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private boolean createConnection() {
+        cs = new ClientSocket(ip, port);
+        try {
+            cs.CreateConnection();
+            System.out.print("连接服务器成功!" + "\n");
+            return true;
+        } catch (Exception e) {
+            System.out.print("连接服务器失败!" + "\n");
+            return false;
+        }
+
+    }
+
+    private void sendMessage() {
+        if (cs == null)
+            return;
+        try {
+            cs.sendMessage(sendMessage);
+        } catch (Exception e) {
+            System.out.print("发送消息失败!" + "\n");
+        }
+    }
+
+    private void getMessage() {
+        if (cs == null)
+            return;
+        DataInputStream inputStream = null;
+        try {
+            inputStream = cs.getMessageStream();
+        } catch (Exception e) {
+            System.out.print("接收消息缓存错误\n");
+            return;
+        }
+
+        try {
+            //本地保存路径，文件名会自动从服务器端继承而来。
+            String savePath = "source/";
+            int bufferSize = 8192;
+            byte[] buf = new byte[bufferSize];
+            int passedlen = 0;
+            long len=0;
+
+            savePath += inputStream.readUTF();
+            DataOutputStream fileOut = new DataOutputStream(new BufferedOutputStream(new BufferedOutputStream(new FileOutputStream(savePath))));
+            len = inputStream.readLong();
+
+            System.out.println("文件的长度为:" + len + "\n");
+            System.out.println("开始接收文件" + "\n");
+
+            while (true) {
+                int read = 0;
+                if (inputStream != null) {
+                    read = inputStream.read(buf);
+                }
+                passedlen += read;
+                if (read == -1) {
+                    break;
+                }
+            }
+            System.out.println("接收完成，文件存为" + savePath + "\n");
+
+            fileOut.close();
+        } catch (Exception e) {
+            System.out.println("接收消息错误" + "\n");
+            return;
+        }
+    }
+
+    public static void main(String arg[]) {
+        new Client();
+    }
+}
---
+++ EXAM2/src/main/java/com/hand/Server.java
@@ -0,0 +1,70 @@
+package com.hand;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    int port = 12345;
+
+    void start() {
+        Socket s = null;
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            while (true) {
+                // 选择进行传输的文件
+                String filePath = "target.pdf";
+                File fi = new File(filePath);
+
+                System.out.println("文件长度:" + (int) fi.length());
+
+                // public Socket accept() throws
+                // IOException侦听并接受到此套接字的连接。此方法在进行连接之前一直阻塞。
+
+                s = ss.accept();
+                System.out.println("建立socket链接");
+                DataInputStream dis = new DataInputStream(new BufferedInputStream(s.getInputStream()));
+                dis.readByte();
+
+                DataInputStream fis = new DataInputStream(new BufferedInputStream(new FileInputStream(filePath)));
+                DataOutputStream ps = new DataOutputStream(s.getOutputStream());
+                ps.writeUTF(fi.getName());
+                ps.flush();
+                ps.writeLong((long) fi.length());
+                ps.flush();
+
+                int bufferSize = 8192;
+                byte[] buf = new byte[bufferSize];
+
+                while (true) {
+                    int read = 0;
+                    if (fis != null) {
+                        read = fis.read(buf);
+                    }
+
+                    if (read == -1) {
+                        break;
+                    }
+                    ps.write(buf, 0, read);
+                }
+                ps.flush();
+                // 注意关闭socket链接哦，不然客户端会等待server的数据过来，
+                // 直到socket超时，导致数据不完整。
+                fis.close();
+                s.close();
+                System.out.println("文件传输完成");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String arg[]) {
+        new Server().start();
+    }
+}
